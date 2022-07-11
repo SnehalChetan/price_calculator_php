2766f63ee713a9d8e23310b1b463284b4df86af5
Greet Verduyn <greetverduyn@proximus.be>
2022-07-11 17:09:26 +0200

    function to find discounts made in words

--- Model/Customer.php
+++ Model/Customer.php
@@ -21,3 +21,15 @@ class Customer
 
     }
 }
+/*function to make
+groups[]= group_id FROM customer + parent_id FROM customer group (if exist)
+    + parent_id FROM customer group (if exist)
+
+From this array, sum of all fixed disount and highest variabel discount)
+Then compare with the personal discounts
+*/
+
+$groups=[];
+$group= "SELECT group_id FROM customer";
+$groups[]=$group;
+while (isset("SELECT parent_id FROM customer_group WHERE ))
