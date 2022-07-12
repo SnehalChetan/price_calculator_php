4d2624a4a39c14fe5f6d8d78597e98c32f1c515d
Greet Verduyn <greetverduyn@proximus.be>
2022-07-12 11:47:55 +0200

    added setters to class CustomerGroups

--- Model/CustomerGroups.php
+++ Model/CustomerGroups.php
@@ -3,7 +3,7 @@
 class CustomerGroups
 
 {
-    public string $id;
+    public int $id;
     public string $name;
     public int $parent_id;
     public int $fixed_discount;
@@ -35,6 +35,32 @@ class CustomerGroups
     public function getVariableDiscount()
     {
         return  $this->variable_discount;
+    }
+
+    // SET METHODS
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function setParentId (int $parent_id)
+    {
+        $this->parent_id = $parent_id;
+    }
+
+    public function setFixedDiscount(int $fixed_discount)
+    {
+        $this->fixed_discount = $fixed_discount;
+    }
+
+    public function setVariableDiscount(int $variable_discount)
+    {
+        $this->variable_discount = $variable_discount;
     }
 
 }
